c6886aedc2a05f9668a0eb71a80908a79f62ad5f
Mike <ivanov_mv@viro.edu.ru>
2022-05-22 10:53:31 +0300

    22 may 2022

--- polls/urls.py
+++ polls/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 app_name = 'polls'
 urlpatterns = [
-    re_path(r'^$', views.index, name='index'),
-    re_path(r'^(?P<question_id>[\d]+)/', include([
+    re_path(r'^$', views.IndexView.as_view(), name='index'),
+    re_path(r'^(?P<pk>[\d]+)/', include([
         re_path(r'^$', views.detail, name='detail'),
         re_path(r'^result/$', views.result, name='result'),
         re_path(r'^vote/$', views.vote, name='vote')
--- polls/views.py
+++ polls/views.py
@@ -1,32 +1,54 @@
-#from django.shortcuts import render
-from re import template
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.template import loader
 from django.shortcuts import render, get_object_or_404
-from .models import Question
+from django.urls import reverse
+from django.views import generic
+from .models import Question, Choise
+
 
 # Create your views here.
 
-def index(request):
-    q_list = Question.objects.order_by('-pub_date')[:5]
-    content = {'q_list': q_list}
-    i_template = loader.get_template('polls/index.html')
-    return HttpResponse(i_template.render(content, request))
+# def index(request):
+#     '''
+#     Page views list of questions
+#     '''
+#     q_list = Question.objects.order_by('-pub_date')[:5]
+#     content = {'q_list': q_list}
+#     i_template = loader.get_template('polls/index.html')
+#     return HttpResponse(i_template.render(content, request))
 
 
-def detail(request, question_id):
-#    try:
-#        question = Question.objects.get(id=question_id)
-#    except Question.DoesNotExist:
-#        raise Http404('Заданный опрос не найден')
-#    content = {'content': question}
-#    return render(request, 'polls/detail.html', content)
-    question = get_object_or_404(Question, id=question_id)
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'q_list'
+
+    def get_queryset(self):
+        return Question.objects.order_by('-pub_date')
+
+
+def detail(request, pk):
+    try:
+        question = Question.objects.get(id=pk)
+    except Question.DoesNotExist:
+        raise Http404('Заданный опрос не найден')
+        content = {'content': question}
+        return render(request, 'polls/detail.html', content)
+    question = get_object_or_404(Question, id=pk)
     return render(request, 'polls/detail.html', {'question': question})
 
-def result(request, question_id):
-    message_text = 'Вы просматриваете результаты опроса %s'
-    return HttpResponse(message_text % question_id)
 
-def vote(request, question_id):
-    return HttpResponse('Вы голосуете в вопросе %s.' % question_id)
+def result(request, pk):
+    question = get_object_or_404(Question, pk=pk)
+    return render(request, 'polls/result.html', {'question': question})
+
+def vote(request, pk):
+    question = get_object_or_404(Question, pk=pk)
+    try:
+        choise = question.choise_set.get(pk=request.POST['choice'])
+    except (KeyError, Choise.DoesNotExist):
+        error_message = 'Необходимо выбрать один из вариантов'
+        return HttpResponseRedirect(reverse('polls:detail', args=(question.id,)), {'questionw': question, 'error_message': error_message})
+    else:
+        choise.votes += 1
+        choise.save()
+        return HttpResponseRedirect(reverse('polls:result', args=(question.id,)))
